5a324aae42d2f0693d38c8667129a6969aaf4f00
limuyan44 <1332729132@qq.com>
2020-08-22 23:53:05 +0800

    depoy

---
+++ v2ex/main.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         v2ex
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  跳转新页面打开!
+// @require file://E:/workspace-js/monkey-script/v2ex/a.js
+// @author       zycat
+// @match        *://*.v2ex.com/*
+// @grant        GM_log
+// @run-at document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+})();
